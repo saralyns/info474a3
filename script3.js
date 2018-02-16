91e1db71bc57877514ccfc15540fed4c9b10412d
saralyns <saralyns@uw.edu>
2018-02-16 15:22:25 -0800

    removed misc files

--- script3.js
+++ script3.js
@@ -1,50 +1,85 @@
 $(function () {
 
-	var drawMap = function () {
-		var map = L.map('mapid').setView([37.09024, -95.712891], 4);
 
-		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+    // Draws map
+    var drawMap = function () {
+        var map = L.map('mapid').setView([37.09024, -95.712891], 4);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-		L.marker([47.6548, -122.30778]).addTo(map)
-		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-		.on("click", function(){
-			openPopup();
-		});
-	}
+        /*L.marker([47.6548, -122.30778]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .on("click", function(){
+            openPopup();
+        });*/
 
-	drawMap();
+        var dataset = []; // keeps track of full dataset
 
 
-	var dataset;
+        getData(dataset, map);
 
-	d3.csv("massshootings.csv", function (error, shootings) {
-		if (error) return console.warn(error);
-		shootings.forEach(function(d) {
-			d.month = +d.month;
-			d.year = +d.year;
-			d.lat = +d.lat;
-			d.lng = +d.lng;
-			d.killed = +d.killed;
-			d.injured = +d.injured;
-		});
-		
-		//dataset is the full dataset -- maintain a copy of this at all times
-		dataset = shootings;
+        /*L.marker([b.LAT, b.LNG]).addTo(map)
+            .bindPopup('On ' + b.DATE + ', ' + b.KILLED + ' people were killed and ' + b.INJURED + ' people were injured in a shooting in ' + b.CITYCTY + ', ' + b.STATE + '.');*/
+    }
 
-		addMarkers(dataset);
-	});
 
-	var addMarkers = function (dataset) {
-		dataset.forEach(function (d) {
-			L.marker([d.lat, d.lng]).addTo(map)
-			.bindPopup('On ' + d.date + ', ' + d.killed + ' people were killed and ' + d.injured + ' people were injured in a shooting in ' + d.citycty + ', ' + d.state + '.')
-			.on("click", function(){
-				openPopup();
-			});
-		});
-	}
 
+    var getData = function(dataset, map) {
+
+        // Reads through CSV and converts values to numbers
+        d3.csv("massshootings.csv", function (error, shootings) {
+            if (error) return console.warn(error);
+
+            shootings.forEach(function(d) {
+                d.MONTH = +d.MONTH;
+                d.YEAR = +d.YEAR;
+                d.LAT = +d.LAT;
+                d.LNG = +d.LNG;
+                d.KILLED = +d.KILLED;
+                d.INJURED = +d.INJURED;
+                // console.log('On ' + d.DATE + ', ' + d.KILLED + ' people were killed and ' + d.INJURED + ' people were injured in a shooting in ' + d.CITYCTY + ', ' + d.STATE + '. LAT:' + d.LAT + ' LNG: ' + d.LNG);
+            });
+
+            console.log(shootings);
+
+            //dataset = full dataset
+            dataset = shootings;
+
+            console.log(dataset);
+            console.log(dataset[0]);
+
+            addMarkers(dataset, map);
+
+        });
+
+    }
+
+
+    // Adds markers to map
+    var addMarkers = function (dataset, map) {
+        console.log(dataset);
+        dataset.forEach(function(d) {
+            // L.marker([d.LAT, d.LNG])
+            circle = L.circle([d.LAT, d.LNG], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: d.INJURED
+            }).addTo(map)
+                .bindPopup('On ' + d.DATE + ', ' + d.KILLED + ' people were killed and ' + d.INJURED + ' people were injured in a shooting in ' + d.CITYCTY + ', ' + d.STATE + '.');
+        });
+    }
+
+    // Sets up an initial marker
+    var init = function(dataset, map) {
+
+        var b = dataset[0];
+        /*L.marker([b.LAT, b.LNG]).addTo(map)
+            .bindPopup('On ' + b.DATE + ', ' + b.KILLED + ' people were killed and ' + b.INJURED + ' people were injured in a shooting in ' + b.CITYCTY + ', ' + b.STATE + '.');*/
+        console.log(b);
+    }
+
+    drawMap();
 
 });
